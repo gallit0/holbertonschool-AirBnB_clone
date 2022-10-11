f24eec97d455f57c16c84ac7faffd391cb3631ce
rrtyui <marcelo6549@gmail.com>
2022-10-11 13:25:23 -0300

    First check console

---
+++ console.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = '(hbnb) '
+  
+    def do_EOF(self, line):
+        return True
+
+    def do_quit(self, args):
+        """Quit command to exit the program"""
+        raise SystemExit
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
