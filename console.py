100f08a28b6c8f6412ed456fb07531096799aa64
rrtyui <marcelo6549@gmail.com>
2022-10-11 13:57:44 -0300

    emptyline implementation

--- console.py
+++ console.py
@@ -8,12 +8,17 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
   
-    def do_EOF(self, line):
+    def do_EOF(self, line,  ):
         return True
 
     def do_quit(self, args):
         """Quit command to exit the program"""
         raise SystemExit
-
+    
+    def emptyline(self):
+        """Do nothing after empty line + enter"""
+        pass
+    
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
