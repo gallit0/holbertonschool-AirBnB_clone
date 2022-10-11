990d302ecaf2a14d453f738bdf2ff23a68f46c85
rrtyui <marcelo6549@gmail.com>
2022-10-11 15:44:43 -0300

    fix storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,6 +32,7 @@ class FileStorage:
         try:
             with open(self.__file_path, 'r') as f:
                 d = json.load(f)
+                FileStorage.__objects = d
                 for key, value in d.items:
                     self.new(eval(value['__class__'])(**value))
         except:
