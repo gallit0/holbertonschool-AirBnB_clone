34010a78b3d59e289c7e21f838c45bef9b753b24
gallit0 <mateogesede@gmail.com>
2022-10-11 08:59:39 -0300

    cases fixed at atsk 3

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.now())
+        self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
