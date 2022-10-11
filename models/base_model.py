b6d561c13842424aab647d3e0263ad90a2473fe3
gallit0 <mateogesede@gmail.com>
2022-10-11 08:56:23 -0300

    first attempt at checker with task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python3
 """Model base class"""
+
+
+import uuid
+
+
+from datetime import datetime
+
+
+class BaseModel:
+    """Base Model class"""
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = str(datetime.now())
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return string of instance"""
+        return f'[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>'
+
+    def save(self):
+        """Saves update time"""
+        self.updated_at = str(datetime.now())
+
+    def to_dict(self):
+        """To dicitonary function"""
+        d = self.__dict__
+        d['__class__'] = self.__class__.__name__
+        return d
