19a474eae8b5bf123f8dc57dcf8655b0602bd152
gallit0 <mateogesede@gmail.com>
2022-10-11 09:13:53 -0300

    task 3 datetime fixed

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at.isoformat()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Return string of instance"""
@@ -28,4 +28,6 @@ class BaseModel:
         """To dicitonary function"""
         d = self.__dict__
         d['__class__'] = self.__class__.__name__
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
         return d
