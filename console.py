64bb360ad8a39e179acd3d2a82d6a3f890ab60a2
gallit0 <mateogesede@gmail.com>
2022-10-12 22:35:59 -0300

    WIP: task 7, added create command

--- console.py
+++ console.py
@@ -4,6 +4,9 @@
 import cmd
 
 
+import models
+
+
 class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
@@ -18,7 +21,18 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing after empty line + enter"""
         pass
-    
-    
+
+    def do_create(self, args):
+        """Create command"""
+        if len(args) < 1:
+            print('** class name missing **')
+            return
+        if args == 'BaseModel':
+            obj = models.base_model.BaseModel()
+        else:
+            print('** class doesn\'t exist **')
+            return
+        print(obj.id)
+ 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
