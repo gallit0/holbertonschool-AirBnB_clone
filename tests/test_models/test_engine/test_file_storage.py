d40e4d320c79326ead47510646dbfff73528bbf1
gallit0 <mateogesede@gmail.com>
2022-10-17 10:43:48 -0300

    tests fixed

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,14 @@ from models.base_model import BaseModel
 
 class FileStorageClass(unittest.TestCase):
     """ Test File Storage """
+    def test_filepath(self):
+        """
+        Checks that the file path is set to file.json
+        """
+        f = FileStorage()
+        a = f._FileStorage__file_path
+        self.assertEqual(a, "file.json")
+
     def test_everything(self):
         storage = FileStorage()
         storage.all().clear()
